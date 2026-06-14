6413fd2eed7ba90d99b1395eebf213096fb24da3
Лена <ippolitovalena77@gmail.com>
2026-06-15 02:54:27 +0300

    5 modul: Add Spring Security admin area

--- src/main/java/com/example/helpdesk/controller/TicketController.java
+++ src/main/java/com/example/helpdesk/controller/TicketController.java
@@ -23,11 +23,11 @@ public class TicketController {
         this.ticketService = ticketService;
     }
 
-    @GetMapping
-    public String showTickets(Model model) {
-        model.addAttribute("tickets", ticketService.getAllTickets());
-        return "tickets";
-    }
+    //@GetMapping
+    //public String showTickets(Model model) {
+    //    model.addAttribute("tickets", ticketService.getAllTickets());
+    //    return "tickets";
+    //}
 
     @GetMapping("/new")
     public String showCreateForm(Model model) {
